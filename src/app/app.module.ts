e4f76cb26a1ab47641f5363aab3b6c7450073c2b
Marshall-Sun <910204019@qq.com>
2020-05-20 21:24:35 +0800

    sourcecode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+import { NzCascaderModule } from 'ng-zorro-antd/cascader';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NtkmeButtonModule } from '@ctrl/ngx-github-buttons';
+
+import { CoursetableComponent } from './course-table/course-table.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursetableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzCascaderModule,
+    NzTableModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzIconModule,
+    NtkmeButtonModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/course-table/course-table.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-course-table',
+  templateUrl: './course-table.component.html',
+  styleUrls: ['./course-table.component.css'],
+})
+
+export class CoursetableComponent {
+
+  constructor(private http: HttpClient) { }
+
+  MY_JSON: any;
+  nzOptions: any;
+  values = [2020, 1, 1];
+  listOfData = [
+    {
+      key: 1,
+      day: [{}, {}, {}, {}, {}, {}, {}],
+    },
+    {
+      key: 2,
+      day: [{}, {}, {}, {}, {}, {}, {}],
+    },
+    {
+      key: 3,
+      day: [{}, {}, {}, {}, {}, {}, {}],
+    },
+    {
+      key: 4,
+      day: [{}, {}, {}, {}, {}, {}, {}],
+    },
+    {
+      key: 5,
+      day: [{}, {}, {}, {}, {}, {}, {}],
+    },
+    {
+      key: 6,
+      day: [{}, {}, {}, {}, {}, {}, {}],
+    },
+  ];
+
+  ngOnInit(): void {
+    this.initSemester()
+    this.initCourse()
+  }
+
+  initSemester(): void {
+    this.http.get('https://raw.githubusercontent.com/Marshall-Sun/CDN/master/coursetable/semesterList.json')
+      .subscribe(data => {
+        this.nzOptions = data;
+      });
+
+    let now = new Date();
+    let period: number;
+    const SPRING_2020 = new Date('2020-02-23 00:00:00.000');
+    const SUMMER_2020 = new Date('2020-06-28 00:00:00.000');
+    const FALL_2020 = new Date('2020-09-06 00:00:00.000');
+
+    if (SPRING_2020 <= now && now < SUMMER_2020) {
+      period = now.getTime() - SPRING_2020.getTime();
+      this.values[1] = 1;
+    } else if (now < FALL_2020) {
+      period = now.getTime() - SUMMER_2020.getTime();
+      this.values[1] = 2;
+    } else {
+      period = now.getTime() - FALL_2020.getTime();
+      this.values[1] = 3;
+    }
+
+    this.values[2] = Math.ceil(period / 1000 / 86400 / 7);
+  }
+
+  initCourse(): void {
+    this.http.get('https://raw.githubusercontent.com/Marshall-Sun/CDN/master/coursetable/myCourse.json')
+      .subscribe(data => {
+        this.MY_JSON = data;
+        this.onChanges(this.values);
+      });
+  }
+
+
+  onChanges(inputValues: number[]): void {
+    this.values = inputValues;
+    for (const item of this.listOfData) {
+      item.day = [{}, {}, {}, {}, {}, {}, {}];
+    }
+
+    for (const course of this.MY_JSON) {
+      if (
+        course.year == this.values[0]
+        && course.semester == this.values[1]
+        && course.week.indexOf(this.values[2]) != -1
+      ) {
+        let courseTime = Math.floor(course.time.split('-')[0] / 2);
+        this.listOfData[courseTime].day[course.day] = course
+      }
+    }
+  }
+}
