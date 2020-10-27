3495d8832e8aaf526c8d4642a155cd3464a630f8
Marshall-Sun <910204019@qq.com>
2020-10-27 20:19:48 +0800

    update title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { CoursetableComponent } from './course-table/course-table.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'coursetable', pathMatch: 'full' },
+  { path: '**', redirectTo: 'coursetable', pathMatch: 'full' },
   { path: 'coursetable', component: NavigationComponent },
-  { path: 'coursetable/angular', component: CoursetableComponent },
+  // { path: 'coursetable/angular', component: CoursetableComponent },
 ];
 
 @NgModule({
